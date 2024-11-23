2399a95596ad9ad25fa02a799b847d3c06b6b9e3
DavidB <david.bunyamin.99@gmail.com>
2024-11-23 22:19:59 +0700

    fix/schema-prisma

--- src/product/product.queries.ts
+++ src/product/product.queries.ts
@@ -4,12 +4,7 @@ const prisma = prismaClient;
 
 export const findProducts = async () => {
     const products = await prisma.product.findMany({
-        include: {
-            category: true,
-            color: true,
-            manufacture: true,
-            volume: true,
-        },
+        include: {},
     });
     return products;
 };
@@ -19,58 +14,10 @@ export const findProductbyID = async (id: number) => {
         where: {
             productID: id,
         },
-        include: {
-            category: true,
-            color: true,
-            manufacture: true,
-            volume: true,
-        },
+        include: {},
     });
     return product;
 };
 
 export const createNewProduct = async (data: any) => {
-    const product = await prisma.product.create({
-        data: {
-            category: {
-                create: {
-                    materialTypes: data.category.materialTypes,
-                    culture: data.category.culture,
-                    userCategory: data.category.userCategory,
-                },
-            },
-            color: {
-                create: {
-                    color: data.color.color,
-                },
-            },
-            manufacture: {
-                create: {
-                    manufacturer: data.manufacture.manufacturer,
-                    description: data.manufacture.description,
-                },
-            },
-            volume: {
-                create: {
-                    height: data.volume.height,
-                    length: data.volume.length,
-                    width: data.volume.width,
-                },
-            },
-        },
-    });
-    return product;
 };
-
-// export const queryDeleteProductbyId = async (id: number) => {
-//     //Delete record category, color, volume, manufacture juga
-//     const product = await findProductbyID(id);
-//     if (!product) {
-//         return;
-//     }
-//     await prisma.product.deleteMany({
-//         where: {
-//             productID: id,
-//         },
-//     });
-// };
